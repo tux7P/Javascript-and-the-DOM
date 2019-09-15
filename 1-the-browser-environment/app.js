1b3a7aedafffa14fa507282bec8f3d6c110fc005
Rsoj <lkwa1687@tuta.io>
2019-09-15 23:46:52 +0200

    add get element by class id

--- 1-the-browser-environment/app.js
+++ 1-the-browser-environment/app.js
@@ -2,6 +2,7 @@ const myHeading = document.getElementById('myHeading');
 const myButton = document.getElementById('myButton');
 const myTextInput = document.getElementById('myTextInput');
 const myList = document.getElementsByTagName('li');
+const errorNotPurple = document.getElementsByClassName('error-not-purple');
 
 myButton.addEventListener('click', () => {
     myHeading.style.color = myTextInput.value;
@@ -9,4 +10,8 @@ myButton.addEventListener('click', () => {
 
 for (let i=0; i<myList.length; i+=1) {
     myList[i].style.color = 'purple';
+}
+
+for (let i=0; i<myList.length; i+=1) {
+    errorNotPurple[i].style.color = 'red';
 }
