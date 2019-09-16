e77fdea5f4c73dbea4ca3f4e722a428f1f13f7b6
Rsoj <lkwa1687@tuta.io>
2019-09-16 22:50:26 +0200

    add query selector all background color

--- 1-the-browser-environment/app.js
+++ 1-the-browser-environment/app.js
@@ -3,6 +3,7 @@ const myButton = document.getElementById('myButton');
 const myTextInput = document.getElementById('myTextInput');
 const myList = document.getElementsByTagName('li');
 const errorNotPurple = document.getElementsByClassName('error-not-purple');
+const evens = document.querySelectorAll('li:nth-child(even)');
 
 myButton.addEventListener('click', () => {
     myHeading.style.color = myTextInput.value;
@@ -12,6 +13,10 @@ for (let i=0; i<myList.length; i+=1) {
     myList[i].style.color = 'purple';
 }
 
-for (let i=0; i<myList.length; i+=1) {
-    errorNotPurple[i].style.color = 'red';
+//for (let i=0; i<myList.length; i+=1) {
+//    errorNotPurple[i].style.color = 'red';
+//}
+
+for (let i=0; i<evens.length; i+=1) {
+    evens[i].style.backgroundColor = 'lightgray';
 }
